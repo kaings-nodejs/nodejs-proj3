328a2f4d15a29f37bd0690234af0e487c1eef44c
willys_tjiawi <willys@huubap.com>
2019-04-03 23:34:49 +0800

    retrieve all content of the products table

--- controllers/shop.js
+++ controllers/shop.js
@@ -27,15 +27,18 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render('shop/index', {
-        prods: rows,
-        pageTitle: 'Shop',
-        path: '/'
-      });
-    })
-    .catch(err => console.log(err));
+  Product.findAll()
+  .then(products => {
+    //console.log(products)
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  })
+  .catch(err => {
+    console.log(err)
+  });
 };
 
 exports.getCart = (req, res, next) => {
