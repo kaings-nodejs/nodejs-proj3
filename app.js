ea42f2529acfd07765cb87baa70676bfdd6b246a
willys_tjiawi <willys@huubap.com>
2019-04-09 15:09:49 +0800

    add one-to-many relationship

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,9 +24,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// generates associations between Product and User. This will generate userId as foreignkey in Product
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'}); // ref: http://docs.sequelizejs.com/manual/associations.html
+User.hasMany(Product); // this is optional, the above is enough. This is basically similar as the above line
 
 // sync() creates model that is defined into connected database
-sequelize.sync()
+sequelize
+.sync({force: true})    // to force recreate all the tables. This will create `userId` column in the newly recreated Product table
 .then(result => {
     console.log(result);
     app.listen(3000);
