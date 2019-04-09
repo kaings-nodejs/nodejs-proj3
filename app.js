0e2c1c08c48a2258c5c2e5e9e2530e4a6c3c99a6
willys_tjiawi <willys@huubap.com>
2019-04-09 15:49:01 +0800

    managing dummy user

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use((req, res, next) => {   // this will be run when there is any incoming request. it is put on top of all routes. All incoming request will trigger this middleware
+    User.findByPk(1)
+    .then(user => {
+        req.user = user;    // store sequelize object "user" into "req.user" so that it can be called/used globally
+        next();     // move to the next middleware
+    })
+    .catch(err => {console.log(err)});
+});
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
